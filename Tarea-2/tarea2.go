c59e6318e59d55c531839c36ff8e1cb2a96d4b6f
Josue-MNK <benjamingiron1@gmail.com>
2018-08-08 18:25:22 -0600

    repo

---
+++ Tarea-2/tarea2.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/tipoCambio/tipoCambioDia", TipoCambioDia).Methods("POST")
+	log.Fatal(http.ListenAndServe(":3002", router))
+}
+
+func TipoCambioDia(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//	docker build -t josuegiron/api-suma-go .
+//	docker run -p 3001:3001 josuegiron/api-suma-go
+//	docker tag josuegiron/api-suma-go josuegiron/api-suma-go:version1
+//  docker push josuegiron/api-suma-go:version1
+//	docker stack deploy -c docker-compose.yml api-suma-go-balanceada
